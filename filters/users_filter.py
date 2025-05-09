7d672e2422c84e4121b7823eade69a0add690a9f
VladimirNTS <vladimirtsabolov@gmail.com>
2025-05-09 20:37:21 +0300

    Initial commit

---
+++ filters/users_filter.py
@@ -0,0 +1,12 @@
+import os
+
+from aiogram.filters import Filter
+from aiogram import types
+
+class OwnerFilter (Filter):
+    
+    async def __call__(self, message: types.Message) -> bool:
+        return message.from_user.id == int(os.getenv('OWNER'))
+
+
+
